535b2def1dfe5b9610a6c6d419abaed31d8d7871
azjiajiajia <2652587085@qq.com>
2020-03-13 16:13:48 +0800

    毕设第一次提交

---
+++ src/main/webapp/js/window.js
@@ -0,0 +1,81 @@
+$(function(){
+    //登录窗口
+    $("#close_btn").click(function () {
+        $("#bg").css({display:"none"});
+        $("#login_window").css({display:"none"});
+        return false;
+    });
+    $("#page_tab_reader_login").click(function () {
+        $("#bg").css({display:"block"});
+        $("#login_window").css({display:"block"});
+        return false;
+    });
+    $("#page_tab_reader_register").click(function () {
+        $("#bg").css({display:"block"});
+        $("#register_window").css({display:"block"});
+        $("#register_window_register").css({"margin-bottom":"0px","margin-left":"200px","height":"25px","width":"100px" });
+        return false;
+    });
+    $("#register_close_btn").click(function () {
+        $("#bg").css({display:"none"});
+        $("#register_window").css({display:"none"});
+        return false;
+    });
+    $("#login_window_register").click(function () {
+        $("#login_window").css({display:"none"});
+        $("#register_window").css({display:"block"});
+        $("#register_window_register").css({"margin-bottom":"0px","margin-left":"200px","height":"25px","width":"100px" });
+        return false;
+    });
+
+    $("#login_submit").click(function () {
+        $.ajax({
+            url:'/system/login',
+            type: 'post',
+            data:JSON.stringify({login_user_id:$("#login_user_id").val(),login_pwd:$("#login_pwd").val()}),
+            dataType:'json',
+            async:'false',
+            success: function(data){
+                alert("1");
+                if(data.type == "success"){
+                    $("#bg").css({display:"none"});
+                    $("#login_window").css({display:"none"});
+                    $("#page_tab_reader_login").css({display:"none"});
+                    $("#page_tab_reader_register").css({display:"none"});
+                    $("#page_tab_lable").css({display:"block"});
+                    $("#user_name").css({display:"block"});
+                }
+            },
+            error:function (data) {
+                alert("x");
+            }
+        });
+    });
+
+    $().moveDivByID("login");
+    $().moveDivByID("register");
+});
+
+jQuery.fn.moveDivByID= function (id){
+    $("#"+id+"_title").mousedown(function(e){
+        $(this).css("cursor","move");
+        var offset= $(this).offset();
+        var x= e.clientX-offset.left;
+        var y= e.clientY-offset.top;
+
+        $(document).bind("mousemove",function(ev) {
+            var ev=ev||window.event;
+            var _x= ev.clientX-x;
+            var _y= ev.clientY-y;
+            $("#"+id+"_window").css({"left":_x+120+"px","top":_y+"px"});
+        });
+    });
+
+    $(document).mouseup(function(){
+        $("#"+id+"_window").css("cursor","default");
+        $(this).unbind("mousemove");
+    });
+};
+
+
+
